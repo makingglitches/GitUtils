370ae820f22e54cc76f6ca0d737084e6b56612a2
makingglitches <john.r.sohn@gmail.com>
2025-05-11 14:50:05 -0400

    ** Added some idx search features ** Still need a unified reader

--- git_idx.py
+++ git_idx.py
@@ -10,6 +10,8 @@ class GitIDX(GitBase):
         if not os.path.exists(filename):
             filename = os.path.join(self.packpath,filename)
 
+        self.filename = filename
+
         f = open(filename, "rb")
 
         idxcontents = f.read()
@@ -77,11 +79,86 @@ class GitIDX(GitBase):
         self.idxsha1  = idxsha1b.hex()
         self.packfilesha1 = packsha1b.hex()
 
+    def search(self,objectid):
+
+        searchstr = bytes.fromhex(objectid)
+        first = searchstr[0]
+
+        char1 = searchstr[0]
+        
+        n1 = self.fanoutable[char1-1] if char1 > 0 else 0
+        n2 = self.fanoutable[char1]
+
+        for i in range(n1,n2):
+            if  self.objectids[i]==searchstr:
+                
+                offs = self.smalloffsets[i]
+                offi = offs[1]
+                
+                if offs[0]:
+                    offi = self.largeoffsets[offi]
+                
+                return (self.filename, offi,i, self.idxsha1,self)
+
+        return None
+                
+
+
+
+    @staticmethod
+    def FromDirectory(repopath:str)->list["GitIDX"]:
+         
+        allidx = []
+
+        repopath = os.path.expanduser(repopath)
+        packpath = os.path.join(repopath,'.git','objects','pack')
+
+
+        for dir in os.walk(packpath):
+
+            for file in dir[2]:                
+                if file.endswith('.idx'):
+                    g = GitIDX(repopath,file)
+                    allidx.append(g)
+        return allidx
+        
+
+def searchindexes(idxs:list[GitIDX], objects:list[str]) ->dict[str,tuple | None]:
+
+    res = {}
+
+    for i in idxs:
+        
+        for o in objects:
+            r = i.search(o)
+            if r is not None:
+                res[o] = r
+                continue
+
+    for o in objects:
+        if not o in res:
+            res[o] = None
+
+    return res               
+
+
 if __name__ == "__main__":
 
     RepoPath = "~/Documents/placeflattener_git/"
     idx = "pack-c44714374edcabd029054f8c20bd601ba55ecfe9.idx"
     g = GitIDX(RepoPath,idx)
+
+    sampleobjectids = [
+                        '021695a1775f22bab5cff36751e7eae5db49b9cf',
+                        '113f49ca85e805debe7a33032aae7d5c9d3f1feb',
+                        '86114c801184bcc472ec296ede8d49c3786a19f0',
+                        '68a72d69a209f65e3ac10f4cc16706e180ed9019',
+                        'ff4d8b40c2a64474660766046e16080c3786c794'
+    ]
+
+    idx:list[GitIDX] = GitIDX.FromDirectory(RepoPath)
+
+    res = searchindexes(idx,sampleobjectids)
 
     print('Passed')
 
