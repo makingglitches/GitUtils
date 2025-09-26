d96c4781359980b1d55135c12287654bdf1bd197
makingglitches <john.r.sohn@gmail.com>
2025-09-26 19:52:03 -0400

    refs #178 * committed missed removal change and updated gitignore.

--- git_tag.py
+++
@@ -1,7 +0,0 @@
-from git_const import GitLocationType, GitObjectType
-from git_locationbase import GitLocationBase
-
-
-class GitTag(GitLocationBase):
-    def __init__(self, repopath, objectId):
-        super().__init__(repopath, objectId, GitObjectType.TAG)
